8a3ef218893391c041bfa3d55913a8195f827df6
Barsha Maharjan <barshamhrzan@gmail.com>
2021-03-15 22:17:13 +0545

    Task 2: Create a RecyclerView

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
@@ -10,15 +12,24 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private final LinkedList<String> myworldList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
         for (int i = 0; i < 20; i++) {
             myworldList.addLast("word" + i);
         }
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mAdapter = new WordListAdapter(this, myworldList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
     }
 
